d45eef3ee871447ee6d1b02aff71bf560e3be29a
Hope <55392976+hhill24@users.noreply.github.com>
2020-03-24 18:46:08 +0000

    partial implementation scheduleAppointment

--- Menu.java
+++ Menu.java
@@ -1,20 +1,24 @@
-import java.util.HashMap;
+
+import java.util.Date;
+import java.util.HashSet;
+
+
+
 import java.util.Scanner;
+import java.util.Set;
 
 public class Menu {
-	HashMap <HealthProfessional, Diary> HPDiaries;
-	HashMap <Patient, Diary> patientDiaries;
+	
 	Scanner s = new Scanner(System.in);
+	Set<Diary> diaries;
 
+	
 	public Menu() {
-		HPDiaries = null;
-		patientDiaries = null;
+		diaries=null;
 	}
 	
 	public void initialise() {
-		HPDiaries = new HashMap<HealthProfessional, Diary>();
-		patientDiaries = new HashMap<Patient, Diary>();
-
+		diaries =new HashSet<>();
 	}
 	
 	public int displayMenu() {
@@ -70,16 +74,78 @@ public class Menu {
 	/**
 	 * searches for an appointment.
 	 */
-	public void search(){
+	
+	
+	public Appointment search(){
+		System.out.println("Enter Appointment ID");
+		int wantedID = s.nextInt();
+		Appointment found = null;
+	
+		for(Diary d: diaries) {
+			found = d.findInDiary(wantedID);
+		}
+		return found;
 		
 	}
 	
 	/**
-	 * schedules an apppointment based on the diaries of the health professionals
+	 * schedules an appointment based on the diaries of the health professionals
 	 */
 	public void scheduleAppointment() {
 		
+		int[] startInt = {0,0,0};
+		int[] endInt = {0,0,0};
+
+		System.out.println("Enter List of Health Professionals involved, separated by commas:");
+		String input = s.next();
+		String[] professionals = input.split(",");
+		
+		System.out.println("Enter start date in format YYYY/MM/DD");
+		String start = s.next();
+		String[] startDateString = start.split("/");
+		System.out.println("Enter end date in format YYYY/MM/DD");
+		String end = s.next();
+		String[] endDateString= end.split("/");
+		
+		
+		for (int i=0; i<3; i++) {
+			startInt[i]=Integer.parseInt(startDateString[i]);
+			endInt[i]=Integer.parseInt(endDateString[i]);
+
+		}
+
+		Date startDate = new Date(startInt[0], startInt[1], startInt[2]);
+		Date endDate = new Date(endInt[0], endInt[1], endInt[2]);
+		
+		//creates a set of the diaries of the needed health professionals
+		Set<Diary> neededHPs = new HashSet<>();
+		for (String hp: professionals) {
+			for(Diary d: diaries) {
+				if (d.getOwner().getName()==hp) {
+					neededHPs.add(d);
+				}
+			}
+		}
+		
+		//creates a set with the dates (between startDate and endDate) that all required professionals are busy
+		Set<Date> allBusy = new HashSet<>();
+		for(Diary d : neededHPs) {
+			Set<Date> b = d.createBusy(d.root);
+			for (Date dd : b) {
+				
+				if(dd.getDate()<endDate.getDate() || dd.getDate()>startDate.getDate()) {
+				allBusy.add(dd);
+				}
+			}
+			
+		}
+		
+		//find earliest date between start and end not in allBusy
+		//add appointment with this date to all health professionals in neededHPs
+		
+
 	}
+	
 	
 	/**
 	 * returns how long the program took to complete a scheduling
