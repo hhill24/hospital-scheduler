cdbbdb50d5224716b57f1312cd56dd0bd1408b39
Hope <55392976+hhill24@users.noreply.github.com>
2020-04-07 22:32:07 +0100

    final version

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -4,14 +4,14 @@
  *
  */
 public class HealthProfessional {
-	int id=0;
+	int id = 0;
 	String name;
 	String profession;
 	String location;
 	Diary appointments;
-	
+
 	/**
-	 * Default constructor that creates a blank instance of Health{rofessional
+	 * Default constructor that creates a blank instance of HealthProfessional
 	 */
 	public HealthProfessional() {
 		id++;
@@ -20,33 +20,62 @@ public class HealthProfessional {
 		location = "";
 		appointments = null;
 	}
-	
+
 	/**
-	 * Gets the name of the HealthProfessional 
+	 * Alternate constructor to set fields to given parameters
+	 * 
+	 * @param theName
+	 * @param job
+	 * @param loc
+	 * @param d
+	 */
+	public HealthProfessional(String theName, String job, String loc, Diary d) {
+		id++;
+		name = theName;
+		profession = job;
+		location = loc;
+		appointments = d;
+	}
+
+	/**
+	 * Gets the unique ID of the HealthProfessional
+	 * 
+	 * @return String name
+	 */
+	public int getID() {
+		return id;
+	}
+
+	/**
+	 * Gets the name of the HealthProfessional
+	 * 
 	 * @return String name
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
-	 * Gets the profession of the HealthProfessional 
+	 * Gets the profession of the HealthProfessional
+	 * 
 	 * @return String profession
 	 */
 	public String getProfession() {
 		return profession;
 	}
-	
+
 	/**
-	 * Gets the location within hospital of the HealthProfessional 
+	 * Gets the location within hospital of the HealthProfessional
+	 * 
 	 * @return String location
 	 */
 	public String getLocation() {
 		return location;
 	}
-	
+
 	/**
-	 * Gets the Diary tree of the HealthProfessional 
+	 * Gets the Diary tree of the HealthProfessional
+	 * 
 	 * @return Diary appointments
 	 */
 	public Diary getAppointments() {
