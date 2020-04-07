1ac215ab51f43e1282dcaefd19a4183e327f217a
Hope <55392976+hhill24@users.noreply.github.com>
2020-04-07 14:15:18 +0100

    added javadoc

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -1,29 +1,54 @@
-
+/**
+ * 
+ * Stores details about individual hospital employees
+ *
+ */
 public class HealthProfessional {
+	int id=0;
 	String name;
 	String profession;
 	String location;
 	Diary appointments;
 	
+	/**
+	 * Default constructor that creates a blank instance of Health{rofessional
+	 */
 	public HealthProfessional() {
+		id++;
 		name = "";
 		profession = "";
 		location = "";
 		appointments = null;
 	}
 	
+	/**
+	 * Gets the name of the HealthProfessional 
+	 * @return String name
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Gets the profession of the HealthProfessional 
+	 * @return String profession
+	 */
 	public String getProfession() {
 		return profession;
 	}
 	
+	/**
+	 * Gets the location within hospital of the HealthProfessional 
+	 * @return String location
+	 */
 	public String getLocation() {
 		return location;
 	}
 	
+	/**
+	 * Gets the Diary tree of the HealthProfessional 
+	 * @return Diary appointments
+	 */
 	public Diary getAppointments() {
 		return appointments;
 	}
