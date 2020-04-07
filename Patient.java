61d8142e26375c89569e8657095caf6c9d2a2d9e
Hope <55392976+hhill24@users.noreply.github.com>
2020-04-07 14:17:18 +0100

    added javadoc comments

--- Patient.java
+++ Patient.java
@@ -1,17 +1,33 @@
 
+/**
+ * 
+ * stores details of each individual patient of the hospital
+ *
+ */
 public class Patient {
 	String name;
 	Diary appointments;
 	
+	/**
+	 * Default constructor to create new blank instance of patient
+	 */
 	public Patient() {
 		name = "";
 		appointments = null;
 	}
 	
+	/**
+	 * Gets the Patient name
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 	
+	/**
+	 * Gets the Diary tree of the Patient 
+	 * @return Diary appointments
+	 */
 	public Diary getAppointments() {
 		return appointments;
 	}
