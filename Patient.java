e8dc726b51bb0eed38e1c344442799d68385ae3a
Hope <55392976+hhill24@users.noreply.github.com>
2020-04-07 22:33:56 +0100

    final version

--- Patient.java
+++ Patient.java
@@ -1,4 +1,3 @@
-
 /**
  * 
  * stores details of each individual patient of the hospital
@@ -7,7 +6,7 @@
 public class Patient {
 	String name;
 	Diary appointments;
-	
+
 	/**
 	 * Default constructor to create new blank instance of patient
 	 */
@@ -15,21 +14,34 @@ public class Patient {
 		name = "";
 		appointments = null;
 	}
-	
+
+	/**
+	 * Alternative constructor to set fields to given parameters
+	 * 
+	 * @param String the name
+	 * @param Diary  d
+	 */
+	public Patient(String theName, Diary d) {
+		name = theName;
+		appointments = d;
+	}
+
 	/**
 	 * Gets the Patient name
+	 * 
 	 * @return
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
-	 * Gets the Diary tree of the Patient 
+	 * Gets the Diary tree of the Patient
+	 * 
 	 * @return Diary appointments
 	 */
 	public Diary getAppointments() {
 		return appointments;
 	}
-	
+
 }
